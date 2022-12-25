8f5a554d6a1be77ff07811c2c7dea67ba876af5d
Vitaliy.Ivashchenko <Vitaly.ivaschenko@mail.ru>
2022-12-25 23:56:25 +0700

    add 3_lesson 3.5HW

---
+++ 3_lesson/3.4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдает таблицу квадратов чисел от 1 до N.
+
+void Pow(int n)
+{
+    int i = 0;
+    while(i < n)
+    {
+        i++;
+        if (i == n)
+            Console.Write($" {Math.Pow(i, 2)}.");
+        else
+            Console.Write($" {Math.Pow(i, 2)},");
+    }
+}
+
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+Pow(N);
---
+++ 3_lesson/3.5 HW/Program.cs
@@ -0,0 +1,23 @@
+﻿// напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// Пример: 14212 ->  нет, 23432 -> да, 12821 -> да
+
+void Palindrome(int Num)
+{
+    if (Num < 99999 && Num >9999 )
+    {
+        if (Num % 10 == Num / 10000 && Num % 100 % 10 == Num / 1000 / 10)
+            Console.WriteLine("да");
+        else
+            Console.WriteLine("нет");
+    }
+
+    else
+    {
+        Console.WriteLine("Error");
+    }
+
+}
+Console.Write("Введите 5-ти значное число: ");
+int Number = int.Parse(Console.ReadLine());
+Palindrome(Number);
+
