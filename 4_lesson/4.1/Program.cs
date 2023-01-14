d4550d1dad3c9eab0a8458631e2e793fb3977356
Vitaliy.Ivashchenko <Vitaly.ivaschenko@mail.ru>
2023-01-14 23:46:32 +0700

    add 4_lesson 4.2

---
+++ 4_lesson/4.1/Program.cs
@@ -0,0 +1,15 @@
+﻿// наишите программу котораяпринимает на вход (А) и выдает сумму чисел от 1 до А.
+// Пример: 7 > 28, 4 > 10, 8 > 36
+
+int SumNums(int num)
+{
+    int all_sum = 0;
+    for (int i = 0; i <= num; i++)
+    {
+        all_sum += i;
+    }
+
+    return all_sum;
+}
+
+Console.WriteLine(SumNums(int.Parse(Console.ReadLine())));
---
+++ 4_lesson/4.2/Program.cs
@@ -0,0 +1,14 @@
+﻿int NumCount(int n)
+{
+    if (n == 0) return (1);
+
+    int count = 0;
+    while (n > 0)
+    {
+        n = n / 10;
+        count++;
+    }
+    return count;
+}
+
+Console.WriteLine(NumCount(int.Parse(Console.ReadLine())));
