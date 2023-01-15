142cce42923c1142be510e9deb3de837411597e1
Vitaliy.Ivashchenko <Vitaly.ivaschenko@mail.ru>
2023-01-15 21:57:30 +0700

    add 5_lesson 5.2

---
+++ 5_lesson/5.2/Program.cs
@@ -0,0 +1,34 @@
+﻿int[] FillArray(int size, int from, int to)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(from, to);
+    }
+    return arr;
+}
+
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] SumNum(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = 0 - arr[i];
+    }
+    return arr;
+}
+Console.WriteLine("Введите количество элементов массива и диапазон: ");
+int[] arr_1 = FillArray(int.Parse(Console.ReadLine()),
+                        int.Parse(Console.ReadLine()),
+                        int.Parse(Console.ReadLine()));
+PrintArray(arr_1);
+PrintArray(SumNum(arr_1));
