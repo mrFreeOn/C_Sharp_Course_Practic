e731684c698e4d545272a7de3f4e5ece436089c2
Vitaliy.Ivashchenko <Vitaly.ivaschenko@mail.ru>
2023-01-16 15:01:11 +0700

    add 5_lesson 5.6hw

--- 5_lesson/5.5/Program.cs
+++ 5_lesson/5.5/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив заполненный случайными положительными трехзначными числами. 
+// Напишите программу, которая покажет количество четных чисел в массиве.
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] FillArray(int size, int from, int to)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(from, to);
+    }
+    return arr;
+}
+
+int Search(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+        if (arr[i] % 2 == 0)
+            count++;
+    return count;
+}
+
+Console.WriteLine("Введите количество элементов массива и диапазон: ");
+int[] arr_1 = FillArray(int.Parse(Console.ReadLine()),
+                        int.Parse(Console.ReadLine()),
+                        int.Parse(Console.ReadLine()));
+PrintArray(arr_1);
+Console.WriteLine(Search(arr_1));
+
