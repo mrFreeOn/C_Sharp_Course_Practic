f29e0f68731e3df86e4347583003bee6a6b46f73
Vitaliy.Ivashchenko <Vitaly.ivaschenko@mail.ru>
2023-01-16 16:59:14 +0700

    add 5_lesson 5.7hw

---
+++ 5_lesson/5.6 HW/Program.cs
@@ -0,0 +1,36 @@
+﻿// задайте одномерный массив заполненный случайными числами. Найдите сумму элементов, стоящих на нечетных позициях.
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] FillArray(int size, int from, int to)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(from, to);
+    }
+    return arr;
+}
+
+int Search(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i += 2)
+        count += arr[i];
+    return count;
+}
+
+Console.WriteLine("Введите количество элементов массива и диапазон: ");
+int[] arr_1 = FillArray(int.Parse(Console.ReadLine()),
+                        int.Parse(Console.ReadLine()),
+                        int.Parse(Console.ReadLine()));
+PrintArray(arr_1);
+Console.WriteLine(Search(arr_1));
+
---
+++ 5_lesson/5.7 HW/Program.cs
@@ -0,0 +1,40 @@
+﻿void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] FillArray(int size, int from, int to)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(from, to);
+    }
+    return arr;
+}
+
+int[] Search(int[] arr)
+{
+    int size = arr.Length;
+    int flex_size = size / 2 + size % 2;
+    int[] result = new int[flex_size];
+
+    for (int i = 0; i < size / 2; i++)
+        result[i] = arr[i] * arr[size - i - 1];
+
+    if (result[flex_size - 1] == 0)
+        result[flex_size - 1] = arr[flex_size - 1];
+    return result;
+}
+
+Console.WriteLine("Введите количество элементов массива и диапазон: ");
+int[] arr_1 = FillArray(int.Parse(Console.ReadLine()),
+                        int.Parse(Console.ReadLine()),
+                        int.Parse(Console.ReadLine()));
+PrintArray(arr_1);
+PrintArray(Search(arr_1));
+
