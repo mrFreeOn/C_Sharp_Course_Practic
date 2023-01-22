beec2d933a77df358d2f6db640404da93f7c5026
Vitaliy.Ivashchenko <Vitaly.ivaschenko@mail.ru>
2023-01-23 00:00:12 +0700

    add 6_lesson 6.4

--- 6_lesson/6.3/Program.cs
+++ 6_lesson/6.3/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*не используя рекурсию, выведите первые N чисел фибоначи.
+Первые два числа фибоначи 0 и 1.*/
+
+void Fibonachi(int x)
+{
+    int a = 0;
+    int b = 1;
+
+    for (int i = 0; i < x; i++)
+    {
+        Console.Write($"{a} ");
+        (a, b) = (b, a + b);
+    }
+}
+
+Fibonachi(int.Parse(Console.ReadLine()));
---
+++ 6_lesson/6.4/Program.cs
@@ -0,0 +1,51 @@
+﻿/*Напишите программу, которая будет создавать копию заданного двумерного
+ массива с помощью поэлементного копирования*/
+
+
+void PrintArray2D(int[,] array)
+{
+    int col = array.GetLength(1);
+    int row = array.GetLength(0);
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+            Console.Write($"{array[i, j]} ");
+        Console.WriteLine();
+    }
+}
+
+int[,] FillArray2D(int row, int col, int from, int to)
+{
+    int[,] arr = new int[row, col];
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < col; j++)
+            arr[i, j] = new Random().Next(from, to);
+
+    return arr;
+}
+
+int[,] Clone(int[,] array)
+{
+    int col = array.GetLength(1);
+    int row = array.GetLength(0);
+    int[,] clone_arr = new int[row, col];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+            clone_arr[i, j] = array[i, j];
+    }
+    return clone_arr;
+}
+
+Console.WriteLine("Введите количество строк и столбцов массива и диапазон: ");
+int[,] arr_1 = FillArray2D(int.Parse(Console.ReadLine()),
+                            int.Parse(Console.ReadLine()),
+                            int.Parse(Console.ReadLine()),
+                            int.Parse(Console.ReadLine()));
+Console.WriteLine("исходный массив");
+PrintArray2D(arr_1);
+Console.WriteLine("новый массив");
+int [,] arr_2 = Clone(arr_1);
+PrintArray2D(arr_2);
+
