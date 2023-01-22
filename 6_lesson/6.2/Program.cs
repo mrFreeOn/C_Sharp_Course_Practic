df9584d36445a39fb05e533ad557b06db39bf965
Vitaliy.Ivashchenko <Vitaly.ivaschenko@mail.ru>
2023-01-22 20:27:34 +0700

    add 6_lesson 6.2

---
+++ 6_lesson/6.2/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Наипшите программу, которая будет реобразовывать десятичное число в двоичное.
+45 > 101101, 3 > 11, 2 > 10*/
+
+string TwoInTen(int x)
+{
+    string result = "";
+    while(x > 0)
+    {
+        result = x % 2 + result;
+        x /= 2;
+    }
+    return result;
+}
+Console.WriteLine(TwoInTen(int.Parse(Console.ReadLine())));
