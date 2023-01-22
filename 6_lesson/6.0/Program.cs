334ccffa33fe3cb1c5693174f1265b34595b911f
Vitaliy.Ivashchenko <Vitaly.ivaschenko@mail.ru>
2023-01-22 16:35:39 +0700

    add new lesson 6 6.0

--- 6_lesson/6.0/Program.cs
+++ 6_lesson/6.0/Program.cs
@@ -1,1 +1,39 @@
-﻿// 
+﻿// наишите программу, которая перевернет одномерный массив 
+//(последний элемент удет на первом месте, а первый на последнем и т.д.)
+
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] FillArray(int size, int from, int to)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(from, to);
+    }
+    return arr;
+}
+
+int Search(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+        if (arr[i] % 2 == 0)
+            count++;
+    return count;
+}
+
+Console.WriteLine("Введите количество элементов массива и диапазон: ");
+int[] arr_1 = FillArray(int.Parse(Console.ReadLine()),
+                        int.Parse(Console.ReadLine()),
+                        int.Parse(Console.ReadLine()));
+PrintArray(arr_1);
+Console.WriteLine(Search(arr_1));
+
