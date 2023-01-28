056815912c160453d23bee7a889d9c8c072691f1
Vitaliy.Ivashchenko <Vitaly.ivaschenko@mail.ru>
2023-01-29 00:00:08 +0700

    add new branch lesson7 & new 7_lesson 7.1

---
+++ 7_lesson/7.1/Program.cs
@@ -0,0 +1,30 @@
+﻿/*создайте двумерный массив m*n и заполните его случайными числами*/
+
+void PrintArray2D(int[,] array)
+{   
+    int row_size = array.GetLength(0);
+    int col_size = array.GetLength(1);
+    
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < col_size; j++)
+            Console.Write($" {array[i, j], 4} ");
+        Console.WriteLine();
+    }
+}
+
+int[,] FillArray2D(int row, int col)
+{
+    int[,] arr = new int[row, col];
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < col; j++)
+            arr[i, j] = i + j;
+
+    return arr;
+}
+
+Console.WriteLine("Введите количество строк и столбцов массива и диапазон: ");
+int[,] arr_1 = FillArray2D(int.Parse(Console.ReadLine()),
+                            int.Parse(Console.ReadLine()));
+
+PrintArray2D(arr_1);
